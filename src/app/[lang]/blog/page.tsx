17f1e77d41b82e0e9d1824fcc629bff2b5a43375
harry <goodgoed@gmail.com>
2023-07-15 14:13:41 +0900

    Fix: url

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -3,58 +3,6 @@ import { allPosts, Post } from 'contentlayer/generated'
 import { Locale } from '@/i18n-config'
 import PostCard from '@/components/post'
 import { compareDesc } from 'date-fns'
-import RSS from 'rss'
-import path from 'path'
-import fs from 'fs'
-
-async function generateRss(lang: Locale) {
-  let options = {
-    title: 'Seungwon An (Harry)',
-    description: '',
-    feed_url: `http://localhost:3000/rss_${lang}.xml`,
-    site_url: 'http://localhost:3000',
-    language: lang,
-    pubDate: new Date()
-  }
-  switch (lang) {
-    case 'en':
-      options.description =
-        'A computer science student who pursues high standards in software'
-      break
-    case 'ko':
-      options.description =
-        '소프트웨어의 높은 기준을 추구하는 컴퓨터 공학생입니다'
-      break
-  }
-  const feed = new RSS(options)
-
-  allPosts.forEach((post) => {
-    if (post.locale === lang) {
-      feed.item({
-        title: post.title,
-        description: post.summary,
-        url: `http://localhost:3000/${post.locale}/blog/${post.slug}`,
-        date: post.date,
-        author: 'Seungwon An (Harry)',
-        categories: [post.category]
-      })
-    }
-  })
-
-  const fullFilePath = path.join(process.cwd(), 'public', `rss_${lang}.xml`)
-
-  if (fs.existsSync(fullFilePath)) {
-    await fs.promises.unlink(fullFilePath)
-  }
-
-  fs.writeFile(fullFilePath, feed.xml({ indent: true }), (err) => {
-    if (err) {
-      console.error('Error: ', err)
-    }
-
-    console.log('RSS feed successfully generated!')
-  })
-}
 
 function getAllPosts(lang: String) {
   const posts = allPosts
@@ -69,7 +17,6 @@ export default async function Page({
 }: {
   params: { lang: Locale }
 }) {
-  await generateRss(lang)
   const posts: Post[] = getAllPosts(lang)
 
   return (
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -4,6 +4,10 @@ import { getLocales } from '@/lib/get-locale'
 import { Locale } from '@/i18n-config'
 import profile from '/public/profile.png'
 import Link from 'next/link'
+import RSS from 'rss'
+import path from 'path'
+import fs from 'fs'
+import { allPosts } from 'contentlayer/generated'
 
 function RSSIcon() {
   return (
@@ -53,11 +57,61 @@ function GithubIcon() {
   )
 }
 
+async function generateRss(lang: Locale) {
+  let options = {
+    title: 'Seungwon An (Harry)',
+    description: '',
+    feed_url: `https://seungwonan.com/rss_${lang}.xml`,
+    site_url: `https://seungwonan.com/${lang}`,
+    language: lang,
+    pubDate: new Date()
+  }
+  switch (lang) {
+    case 'en':
+      options.description =
+        'A computer science student who pursues high standards in software'
+      break
+    case 'ko':
+      options.description =
+        '소프트웨어의 높은 기준을 추구하는 컴퓨터 공학생입니다'
+      break
+  }
+  const feed = new RSS(options)
+
+  allPosts.forEach((post) => {
+    if (post.locale === lang) {
+      feed.item({
+        title: post.title,
+        description: post.summary,
+        url: `https://seungwonan.com/${post.locale}/blog/${post.slug}`,
+        date: post.date,
+        author: 'Seungwon An (Harry)',
+        categories: [post.category]
+      })
+    }
+  })
+
+  const fullFilePath = path.join(process.cwd(), 'public', `rss_${lang}.xml`)
+
+  if (fs.existsSync(fullFilePath)) {
+    await fs.promises.unlink(fullFilePath)
+  }
+
+  fs.writeFile(fullFilePath, feed.xml({ indent: true }), (err) => {
+    if (err) {
+      console.error('Error: ', err)
+    }
+
+    console.log('RSS feed successfully generated!')
+  })
+}
+
 export default async function Page({
   params: { lang }
 }: {
   params: { lang: Locale }
 }) {
+  await generateRss(lang)
   const locales = await getLocales(lang)
 
   return (
